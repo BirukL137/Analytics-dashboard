2333c1fb96681b27264b77939543b7a58183a076
BirukL137 <buraf13@gmail.com>
2025-11-20 11:43:42 +0300

    Adding dashboard charts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,8 @@
 import StatCard from "../components/ui/StatCard";
 import { stats } from "../data/stats";
+import LineChartBox from "../components/charts/LineChartBox";
+import BarChartBox from "../components/charts/BarChartBox";
+import AreaChartBox from "../components/charts/AreaChartBox";
 
 const Dashboard = () => {
   return (
@@ -8,6 +11,12 @@ const Dashboard = () => {
         {stats.map((item) => (
           <StatCard key={item.id} title={item.title} value={item.value} />
         ))}
+      </div>
+
+      <div className="dashboard-charts">
+        <LineChartBox />
+        <BarChartBox />
+        <AreaChartBox />
       </div>
     </>
   );
